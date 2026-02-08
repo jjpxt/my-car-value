3a4fdfb8ca60bc39931814494a001accf030d37d
jjpxt <jotajunior.peixoto@gmail.com>
2026-02-08 19:17:22 -0300

    e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,51 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 import { Report } from './reports/reports.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { APP_PIPE } from '@nestjs/core';
+
+const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true
-    }),
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `.env.${process.env.NODE_ENV}`
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      return {
+        type: 'sqlite',
+        database: config.get<string>('DB_NAME'),
+        synchronize: true,
+        entities: [User, Report]
+      }
+    }
+  }),
     UsersModule,
     ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule { }
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: ['KryPTo']
+    }),
+    ).forRoutes('*');
+  }
+}
